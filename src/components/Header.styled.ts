a32a1b28a4d8a21dac218b9ae52607aa528599cf
Sophia (Turner) <me@sophia-dev.io>
2024-01-29 03:02:39 +0900

    feat(theme): add theme toggle button (#4)

---
+++ src/components/Header.styled.ts
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled'
+
+export const Root = styled.nav`
+  margin-bottom: ${({ theme }) => theme.spacing(2)};
+
+  display: flex;
+  justify-content: flex-end;
+`
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { getInitColorSchemeScript } from '@mui/material/styles'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        {getInitColorSchemeScript()}
         <Main />
         <NextScript />
       </body>
