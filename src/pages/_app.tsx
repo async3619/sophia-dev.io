f1445e04afd3dfb75b906398a425bf7d4b7eeb2e
Turner <me@sophia-dev.io>
2024-01-29 13:42:13 +0900

    feat(channel-talk): install channel talk launcher button into website

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import type { AppProps } from 'next/app'
 import { useRemoteRefresh } from 'next-remote-refresh/hook'
+
+import * as ChannelService from '@channel.io/channel-web-sdk-loader'
 
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 
@@ -10,6 +12,11 @@ import { theme } from '@styles/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
   useRemoteRefresh({ shouldRefresh: () => true })
+
+  useEffect(() => {
+    ChannelService.loadScript()
+    ChannelService.boot({ pluginKey: 'eb4b2c86-9a99-4081-8c2d-9897be690491' })
+  }, [])
 
   return (
     <CssVarsProvider theme={theme}>
