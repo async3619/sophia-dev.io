3434042b40618f6b192d786898e66a1cbd803055
Turner <me@sophia-dev.io>
2024-01-29 13:55:20 +0900

    refactor(channel-talk): make channel talk application appearance synced with website theme

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,9 @@
+import React, { useEffect } from 'react'
+
 import { Container, CssBaseline } from '@mui/material'
+import { useColorScheme } from '@mui/material/styles'
+
+import * as ChannelService from '@channel.io/channel-web-sdk-loader'
 
 import { Footer } from '@components/Footer'
 import { Header } from '@components/Header'
@@ -11,6 +16,19 @@ export interface LayoutProps {
 }
 
 export function Layout({ children }: LayoutProps) {
+  const { mode } = useColorScheme()
+
+  useEffect(() => {
+    ChannelService.loadScript()
+    ChannelService.boot({ pluginKey: 'eb4b2c86-9a99-4081-8c2d-9897be690491' })
+
+    if (mode === 'dark') {
+      ChannelService.setAppearance('dark')
+    } else {
+      ChannelService.setAppearance('light')
+    }
+  }, [mode])
+
   return (
     <Styled.Root>
       <CssBaseline />
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,6 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import type { AppProps } from 'next/app'
 import { useRemoteRefresh } from 'next-remote-refresh/hook'
-
-import * as ChannelService from '@channel.io/channel-web-sdk-loader'
 
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 
@@ -12,11 +10,6 @@ import { theme } from '@styles/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
   useRemoteRefresh({ shouldRefresh: () => true })
-
-  useEffect(() => {
-    ChannelService.loadScript()
-    ChannelService.boot({ pluginKey: 'eb4b2c86-9a99-4081-8c2d-9897be690491' })
-  }, [])
 
   return (
     <CssVarsProvider theme={theme}>
