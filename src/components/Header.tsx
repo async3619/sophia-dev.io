77ad801a6280162a610445ee7fbdacb512d630a0
Sophia (Turner) <me@sophia-dev.io>
2024-01-31 04:13:55 +0900

    refactor(nav): add website-wide navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,21 @@
-import * as Styled from './Header.styled'
+import { Stack } from '@mui/material'
 
 import { ThemeButton } from '@components/ThemeButton'
 import { LangSwitch } from '@components/LangSwitch'
+import { Navigator } from '@components/Navigator'
+
+import * as Styled from './Header.styled'
 
 export interface HeaderProps {}
 
 export function Header({}: HeaderProps) {
   return (
     <Styled.Root>
-      <LangSwitch />
-      <ThemeButton />
+      <Navigator />
+      <Stack spacing={1} direction="row" alignItems="center">
+        <LangSwitch />
+        <ThemeButton />
+      </Stack>
     </Styled.Root>
   )
 }
---
+++ src/components/Navigator.styled.ts
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled'
+import Link from 'next/link'
+import { linkInterpolation } from '@styles/link'
+
+export const Item = styled(Link)`
+  ${linkInterpolation}
+`
