c7934585fc692fede88b753bc810a4c29e6b12d3
Sophia (Turner) <me@sophia-dev.io>
2024-01-31 04:32:21 +0900

    refactor(seo): add page assets such as favicons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import Head from 'next/head'
 import { useRemoteRefresh } from 'next-remote-refresh/hook'
 
 import { Analytics } from '@vercel/analytics/react'
@@ -13,8 +15,34 @@ import { theme } from '@styles/theme'
 export default function App({ Component, pageProps }: AppProps) {
   useRemoteRefresh({ shouldRefresh: () => true })
 
+  const { asPath } = useRouter()
+
   return (
     <CssVarsProvider theme={theme}>
+      <Head>
+        <link rel="canonical" href={`https://sophia-dev.io${asPath}`} />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+        <meta name="msapplication-TileColor" content="#000000" />
+        <meta name="theme-color" content="#000000" />
+      </Head>
       <Layout>
         <Component {...pageProps} />
         <Analytics />
