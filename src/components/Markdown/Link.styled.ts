dc8befd90943171b275b845cb7ff1ae4bdae9be0
Sophia (Turner) <me@sophia-dev.io>
2024-02-29 01:37:40 +0900

    refactor(markdown): make relative links to use nextjs `<Link />` component

--- src/components/Markdown/Link.styled.ts
+++ src/components/Markdown/Link.styled.ts
@@ -1,7 +1,13 @@
+import Link from 'next/link'
+
 import styled from '@emotion/styled'
 
 import { linkInterpolation } from '@styles/link'
 
 export const Root = styled.a`
+  ${linkInterpolation}
+`
+
+export const LinkRoot = styled(Link)`
   ${linkInterpolation}
 `
--- src/components/Markdown/Link.tsx
+++ src/components/Markdown/Link.tsx
@@ -5,5 +5,10 @@ import * as Styled from './Link.styled'
 export function MarkdownLink(
   props: React.AnchorHTMLAttributes<HTMLAnchorElement>,
 ) {
+  const { href } = props
+  if (href && href.startsWith('/')) {
+    return <Styled.LinkRoot {...props} href={href} />
+  }
+
   return <Styled.Root {...props} />
 }
