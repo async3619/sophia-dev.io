92b5cf9d9ebb63547cec3870d69e7223ec9e601d
Sophia (Turner) <me@sophia-dev.io>
2024-03-23 22:53:10 +0900

    refactor(movies): improve styles of movie review list item

--- src/components/Review/MovieItem.styled.ts
+++ src/components/Review/MovieItem.styled.ts
@@ -1,20 +1,32 @@
 import Link from 'next/link'
 import styled from '@emotion/styled'
 
-export const Root = styled(Link)`
-  display: block;
-  text-decoration: none;
-`
-
 export const Graphics = styled.div`
   aspect-ratio: 1 / 1;
+
+  display: flex;
+  justify-content: center;
 `
 
 export const Poster = styled.img`
-  width: 100%;
   height: 100%;
 
   display: block;
 
   object-fit: contain;
+
+  box-shadow: ${({ theme }) => theme.shadows[0]};
+  transition: ${({ theme }) =>
+    theme.transitions.create(['box-shadow'], { duration: 450 })};
+`
+
+export const Root = styled(Link)`
+  display: block;
+  text-decoration: none;
+
+  &:hover {
+    ${Poster} {
+      box-shadow: ${({ theme }) => theme.shadows[8]};
+    }
+  }
 `
