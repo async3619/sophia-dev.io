19852d1ce3af50c15d2b247f5cb1cadec839fad7
Sophia (Turner) <me@sophia-dev.io>
2024-03-31 12:07:16 +0900

    refactor(nav): make nav bar can be sticked and applying new design

--- src/components/Header.styled.ts
+++ src/components/Header.styled.ts
@@ -1,9 +1,55 @@
 import styled from '@emotion/styled'
 
-export const Root = styled.nav`
+export const STICKY_OFFSET = 2
+
+interface Props {
+  stick: boolean
+}
+
+export const Root = styled.nav<Props>`
+  padding: ${({ theme }) => theme.spacing(1, 2)};
+
+  margin-left: ${({ theme }) => theme.spacing(-2)};
+  margin-right: ${({ theme }) => theme.spacing(-2)};
   margin-bottom: ${({ theme }) => theme.spacing(2)};
+
+  border-radius: ${({ theme }) => theme.shape.borderRadius * 2}px;
+
+  position: sticky;
+  top: ${({ theme }) => theme.spacing(STICKY_OFFSET)};
+
+  z-index: ${({ theme }) => theme.zIndex.appBar};
 
   display: flex;
   align-items: center;
   justify-content: space-between;
+
+  background-color: ${({ theme }) =>
+    `rgba(${theme.vars.palette.background.defaultChannel} / 0.5)`};
+  box-shadow: ${({ theme, stick }) => (stick ? theme.shadows[16] : 'none')};
+
+  backdrop-filter: ${({ stick }) =>
+    stick ? 'blur(20px) saturate(2.5)' : 'none'};
+
+  transition: ${({ theme }) =>
+    theme.transitions.create([
+      'border-color',
+      'box-shadow',
+      'backdrop-filter',
+    ])};
+
+  ${({ theme }) => theme.getColorSchemeSelector('dark')} {
+    border: 1px solid
+      ${({ stick }) => (stick ? 'rgba(80, 80, 80, 1)' : 'transparent')};
+  }
+
+  ${({ theme }) => theme.getColorSchemeSelector('light')} {
+    border: 1px solid
+      ${({ theme, stick }) => (stick ? theme.palette.divider : 'transparent')};
+  }
+
+  > a:first-child {
+    color: ${({ theme }) => theme.palette.text.primary};
+    text-decoration: none;
+  }
 `
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,49 @@
-import { Stack } from '@mui/material'
+import { Stack, Typography, useTheme } from '@mui/material'
 
 import { ThemeButton } from '@components/ThemeButton'
 import { LangSwitch } from '@components/LangSwitch'
-import { Navigator } from '@components/Navigator'
 
 import * as Styled from './Header.styled'
+import { useEffect, useState } from 'react'
+import { STICKY_OFFSET } from './Header.styled'
+import Link from 'next/link'
+
+const PAGE_DOMAIN = 'sophia-dev.io'
 
 export interface HeaderProps {}
 
 export function Header({}: HeaderProps) {
+  const theme = useTheme()
+  const [isStick, setIsStick] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const offset = window.scrollY
+      const isStick = offset >= parseInt(theme.spacing(STICKY_OFFSET), 10)
+
+      setIsStick(isStick)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+    handleScroll()
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [theme])
+
   return (
-    <Styled.Root>
-      <Navigator />
+    <Styled.Root stick={isStick}>
+      <Link href="/">
+        <Typography
+          component="span"
+          variant="body1"
+          fontFamily="'Consolas', monospace"
+          fontWeight={800}
+        >
+          {PAGE_DOMAIN}
+        </Typography>
+      </Link>
       <Stack spacing={1} direction="row" alignItems="center">
         <LangSwitch />
         <ThemeButton />
