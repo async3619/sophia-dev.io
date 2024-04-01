246827d99ea39df5b72f0eb17768eb423224db4e
Sophia (Turner) <me@sophia-dev.io>
2024-04-01 11:09:43 +0900

    refactor(ui): reduce main layout vertical padding on mobile

--- src/components/Layout.styled.ts
+++ src/components/Layout.styled.ts
@@ -2,7 +2,13 @@ import styled from '@emotion/styled'
 import { css } from '@emotion/react'
 
 export const Root = styled.div`
-  padding: ${({ theme }) => theme.spacing(4, 0)};
+  ${({ theme }) => theme.breakpoints.up('sm')} {
+    padding: ${({ theme }) => theme.spacing(4, 0)};
+  }
+
+  ${({ theme }) => theme.breakpoints.down('sm')} {
+    padding: ${({ theme }) => theme.spacing(1, 0)};
+  }
 `
 
 export const Main = styled.main`
