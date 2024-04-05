84dd880a252cedab5ae5f106e826cf4f3f5d082e
Sophia (Turner) <me@sophia-dev.io>
2024-04-06 03:26:15 +0900

    refactor(mdx): remove code block language label

--- src/components/Markdown/CodeBlock.styled.ts
+++ src/components/Markdown/CodeBlock.styled.ts
@@ -13,19 +13,4 @@ export const Root = styled.pre`
   ${({ theme }) => theme.breakpoints.down('sm')} {
     margin: ${({ theme }) => theme.spacing(2, 0)};
   }
-
-  &:before {
-    content: attr(data-language);
-
-    padding: ${({ theme }) => theme.spacing(0.25, 0.75)};
-    border-bottom-left-radius: ${({ theme }) => theme.shape.borderRadius}px;
-
-    position: absolute;
-    top: 0;
-    right: 0;
-
-    font-size: 0.75em;
-
-    background-color: rgba(255, 255, 255, 0.125);
-  }
 `
