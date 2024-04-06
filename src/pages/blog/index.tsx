65d0f2b141f6a0077c7da1c079db7a2dcf406df8
Sophia (Turner) <me@sophia-dev.io>
2024-04-07 02:01:12 +0900

    refactor(blogs): make blog list to show simplified list and to be able to select detailed

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import _ from 'lodash'
 import dayjs from 'dayjs'
 
@@ -6,10 +7,12 @@ import useTranslation from 'next-translate/useTranslation'
 
 import { Title } from '@components/Title'
 import { BlogList } from '@components/BlogList'
+import { SimpleBlogList } from '@components/SimpleBlogList'
 
 import { BLOG_POST_METADATA_VALIDATOR, BlogPostDocument } from '@constants/blog'
 
 import { getDocuments } from '@utils/getDocuments'
+import { Box, Checkbox, FormControlLabel, Typography } from '@mui/material'
 
 interface BlogsPage {
   blogs: BlogPostDocument[]
@@ -17,11 +20,48 @@ interface BlogsPage {
 
 export default function Blogs({ blogs }: BlogsPage) {
   const { t } = useTranslation('blog')
+  const [detailed, setDetailed] = React.useState(false)
+
+  const handleDetailedChange = React.useCallback(
+    (_: unknown, checked: boolean) => {
+      setDetailed(checked)
+    },
+    [],
+  )
 
   return (
     <div>
-      <Title>{t('title')}</Title>
-      <BlogList blogs={blogs} emptyText={t('empty')} />
+      <Box
+        mb="1.3125rem"
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Title withoutMargin>{t('title')}</Title>
+        <FormControlLabel
+          control={<Checkbox onChange={handleDetailedChange} size="small" />}
+          label={t('detailed')}
+          sx={{ mr: 0 }}
+          componentsProps={{
+            typography: {
+              variant: 'body2',
+              color: 'text.secondary',
+              sx: {
+                userSelect: 'none',
+              },
+            },
+          }}
+        />
+      </Box>
+      {blogs.length === 0 ? (
+        <Typography variant="body2" color="text.secondary" textAlign="center">
+          {t('empty')}
+        </Typography>
+      ) : detailed ? (
+        <BlogList blogs={blogs} />
+      ) : (
+        <SimpleBlogList blogs={blogs} />
+      )}
     </div>
   )
 }
