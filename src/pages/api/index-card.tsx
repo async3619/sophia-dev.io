b9e5c8426fa48ec1a7b448731a3cb4e3a469707a
Sophia (Turner) <me@sophia-dev.io>
2024-04-07 18:46:36 +0900

    refactor(card): apply redesigned index card image

--- src/pages/api/index-card.tsx
+++ src/pages/api/index-card.tsx
@@ -6,66 +6,55 @@ export const config = {
 }
 
 export default async function handler(request: NextRequest) {
-  const { searchParams } = new URL(request.url)
   const suiteRegular = await fetch(
     new URL('./SUITE-Regular.ttf', import.meta.url),
   ).then((res) => res.arrayBuffer())
 
-  const locale = searchParams.get('locale') ?? 'ko'
-  const pageTitle = locale === 'ko' ? '개발자 /소피아/' : 'Sophia, a /webdev/'
+  const suiteExtraBold = await fetch(
+    new URL('./SUITE-ExtraBold.ttf', import.meta.url),
+  ).then((res) => res.arrayBuffer())
 
   return new ImageResponse(
     (
       <div
         style={{
-          backgroundColor: '#1b1b1b',
-          color: '#fff',
+          backgroundImage:
+            'url(https://res.cloudinary.com/dh9u8gpy1/image/upload/v1712481486/card-bg.png)',
           display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'flex-start',
+          justifyContent: 'flex-end',
           height: '100%',
           width: '100%',
-          padding: '2rem',
-          alignItems: 'center',
-          justifyContent: 'center',
+          padding: '48px',
         }}
       >
-        <div
+        <h1
           style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
+            fontSize: '48px',
+            fontWeight: 'bold',
+            margin: 0,
           }}
         >
-          <p
-            style={{
-              fontSize: '4rem',
-              margin: 0,
-              color: 'rgba(255, 255, 255, 0.75)',
-            }}
-          >
-            {pageTitle}
-          </p>
-          <p
-            style={{
-              fontSize: '4rem',
-              marginTop: 0,
-              marginBottom: 0,
-              marginLeft: '1rem',
-              color: 'rgba(255, 255, 255, 0.75)',
-            }}
-          >
-            🌧
-          </p>
-        </div>
+          개발자 /소피아/
+        </h1>
       </div>
     ),
     {
       width: 1200,
       height: 630,
-      emoji: 'blobmoji',
       fonts: [
         {
           style: 'normal',
           data: suiteRegular,
+          weight: 400,
+          lang: 'ko-KR',
+          name: 'suite',
+        },
+        {
+          style: 'normal',
+          data: suiteExtraBold,
+          weight: 800,
           lang: 'ko-KR',
           name: 'suite',
         },
