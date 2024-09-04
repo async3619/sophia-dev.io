4bb84a02bf03dd13c227f9ea515ed45c75398ac7
Sophia (Turner) <me@sophia-dev.io>
2024-09-04 10:53:36 +0900

    refactor(resume): correct content word-breaking styles

--- src/components/Resume/ResumeSection.styled.ts
+++ src/components/Resume/ResumeSection.styled.ts
@@ -1,6 +1,8 @@
 import styled from '@emotion/styled'
 
 export const Root = styled.section`
+  word-break: keep-all;
+
   &:not(:last-of-type) {
     margin-bottom: ${({ theme }) => theme.spacing(4)};
   }
