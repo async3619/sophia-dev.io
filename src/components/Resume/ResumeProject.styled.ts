4afcfc4ab007bd9373babbf2d0fad02029d8183b
Sophia (Turner) <me@sophia-dev.io>
2024-11-30 15:15:50 +0900

    fix(resume): fix a bug that links are shown with wrong color in resume page

--- src/components/Resume/ResumeProject.styled.ts
+++ src/components/Resume/ResumeProject.styled.ts
@@ -1,12 +1,9 @@
 import styled from '@emotion/styled'
-import { linkInterpolation } from '@styles/link'
 
 export const Root = styled.div``
 
 export const Link = styled.a`
-  ${linkInterpolation};
-
-  color: ${({ theme }) => theme.palette.text.primary};
+  color: ${({ theme }) => theme.vars.palette.text.primary};
 
   > span {
     text-decoration: underline;
